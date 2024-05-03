32248a01592ba63a42df5f92666048bbfd06fa34
Vovuchoooo <vity-2005@Mail.ru>
2024-05-03 03:56:15 +0300

    Add Header. Add modal window registration/authorization

---
+++ app/js/modal.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    let modal = document.getElementById('modal_btn');
+    let modal_form = document.getElementById('modal_form');
+    let modalContainer = document.getElementById('modalContainer');
+
+    modal.addEventListener('click', () => {
+        console.log('click');
+        modal_form.classList.remove('modal_disable');
+        modalContainer.classList.add('form_active');
+    });
+
+    modal_form.addEventListener('click', (event) => {
+        console.log('кликнул по заднему фону');
+        if(event.target !== modal_form) {
+
+        }
+        else {
+            modalContainer.classList.remove('form_active');
+            modal_form.classList.add('modal_disable');
+        }
+    });
+
+    let regBtn = document.getElementById('registration_mode');
+    let authBtn = document.getElementById('authorization_mode');
+    let regForm = document.getElementById('registration');
+    let authForm = document.getElementById('authorization')
+
+    regBtn.addEventListener('click', () => {
+        console.log('reg')
+        regBtn.classList.add('active')
+        authBtn.classList.remove('active')
+        authForm.classList.add('disable')
+        regForm.classList.remove('disable')
+
+    })
+
+    authBtn.addEventListener('click', () => {
+        console.log('auth')
+        authBtn.classList.add('active')
+        regBtn.classList.remove('active')
+        authForm.classList.remove('disable')
+        regForm.classList.add('disable')
+    })
+
+    let searchContainer = document.getElementById('searchContainer');
+
+    searchContainer.addEventListener('click', (event) => {
+        if(event.target !== searchContainer) {
+            searchContainer.classList.remove('expanded')
+        }
+        else {
+            searchContainer.classList.add('expanded')
+        }
+    })
+
+});
+
+
+
